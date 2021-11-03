f7c3cebea43499804d42cf2ec7909d54ade70f60
Tania Tasmin <taniat.kaau@email.com>
2021-11-03 13:09:27 -0400

    Create a file play.js which will work like a client setup for snake game

---
+++ play.js
@@ -0,0 +1,26 @@
+// This will serve as your main file through which you will launch the game client.
+
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host:'165.227.47.243'  ,// IP address here,
+    port:50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // client.js
+  conn.on('data', (data) => {
+  console.log('Server says: ', data);
+  });
+  
+  return conn;
+};
+
+
+
+console.log("Connecting ...");
+connect("Hi Tania");
