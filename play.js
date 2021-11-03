e8b5f6b307e6c13baf27433acedf384e712515d1
Tania Tasmin <taniat.kaau@email.com>
2021-11-03 15:48:15 -0400

    Modify play.js to include event listeners for keyboard input

--- play.js
+++ play.js
@@ -1,9 +1,33 @@
 // This will serve as your main file through which you will launch the game client.
 
 const net = require("net");
-const {connect} =require('./client.js');
-
+const {connect} = require('./client.js');
 
 
 console.log("Connecting ...");
 connect();
+
+
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+  process.exit();
+}
+  };
+  return stdin;
+};
+
+setupInput();
