0d4735665e129c6acac56d3abae256a8005fcf68
Tania Tasmin <taniat.kaau@email.com>
2021-11-03 14:37:44 -0400

    Modify client.js to include move commands

--- client.js
+++ client.js
@@ -2,7 +2,7 @@
 
 const net = require("net");
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host:'165.227.47.243'  ,// IP address here,
     port:50541 // PORT number here,
@@ -12,16 +12,33 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on('connect', () => {
-  console.log("Successfully connected to game server");
-  conn.write('Name: TAN!'); // writing to server
+    console.log("Successfully connected to game server");
+    conn.write('Name: TAN!'); // writing to server
   });
+
+  conn.on('connect', () => {
+    let b = 0;
+
+    for (let i = 0; i < 20; i++) {
+      setTimeout(()=> conn.write("Move: right"), 50 * i);
+      b = 50 * i;
+    }
+   
+    for (let i = 0; i < 20; i++) {
+      setTimeout(()=> conn.write("Move: up"), b + 50 * i);
+      b = b + 50 * i;
+    }
+   
+   
+   
+  });// end of connect event to move
   
  
   conn.on('data', (data) => {
-  console.log('Server says: ', data);// data from server
+    console.log('Server says: ', data);// data from server
   });
   
   return conn;
 };// End of Connect function
 
-  module.exports = {connect};
+module.exports = {connect};
