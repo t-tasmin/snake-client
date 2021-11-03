3da59fb2aea672b942c9a85a467f48a5d088e2d5
Tania Tasmin <taniat.kaau@email.com>
2021-11-03 16:55:31 -0400

    Modify play.js to import function setupInput

--- play.js
+++ play.js
@@ -1,33 +1,11 @@
 // This will serve as your main file through which you will launch the game client.
-
 const net = require("net");
-const {connect} = require('./client.js');
+const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 
 console.log("Connecting ...");
 connect();
 
-
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-
-  const handleUserInput = function (key) {
-    if (key === '\u0003') {
-  process.exit();
-}
-  };
-  return stdin;
-};
-
+//Taking inputs from Keyboard
 setupInput();
